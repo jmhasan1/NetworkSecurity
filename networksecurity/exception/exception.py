1db13ffff573dd05b3198691bba48e2c7a502532
Jahid Hasan <jmhasan17@gmail.com>
2026-02-22 01:37:12 +0530

    Data Transformation

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -1,18 +1,37 @@
-import sys
+# import sys
 from networksecurity.logging import logger
 
-class NetworkSecurityException(Exception):
-    def __init__(self,error_message,error_details:sys):
-        self.error_message = error_message
-        _,_,exc_tb = error_details.exc_info()
+# class NetworkSecurityException(Exception):
+#     def __init__(self,error_message,error_details:sys):
+#         self.error_message = error_message
+#         _,_,exc_tb = error_details.exc_info()
         
-        self.lineno=exc_tb.tb_lineno
-        self.file_name=exc_tb.tb_frame.f_code.co_filename 
+#         self.lineno=exc_tb.tb_lineno
+#         self.file_name=exc_tb.tb_frame.f_code.co_filename 
+    
+#     def __str__(self):
+#         return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+#         self.file_name, self.lineno, str(self.error_message))
+        
+
+import sys
+
+class NetworkSecurityException(Exception):
+    def __init__(self, error_message, error_details: sys):
+        # Call the base Exception class with the message
+        super().__init__(error_message) 
+        
+        self.error_message = error_message
+        _, _, exc_tb = error_details.exc_info()
+        
+        self.lineno = exc_tb.tb_lineno
+        self.file_name = exc_tb.tb_frame.f_code.co_filename 
     
     def __str__(self):
-        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
-        self.file_name, self.lineno, str(self.error_message))
-        
+        return "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
+            self.file_name, self.lineno, str(self.error_message))
+
+
 # if __name__=='__main__':
 #     try:
 #         logger.logging.info("Enter the try block")
